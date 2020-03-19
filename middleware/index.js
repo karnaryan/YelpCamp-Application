2dd7fc90de3e0840d00f47d922312e9f31e64c69
Aryan Karn <54681921+arkarn04@users.noreply.github.com>
2020-03-19 19:10:18 +0530

    Added middleware and models

---
+++ middleware/index.js
@@ -0,0 +1,64 @@
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+
+
+var middlewareObj = {};
+
+middlewareObj.checkCampgroundOwnership = function(req,res,next){
+    //is user logged in?
+   if(req.isAuthenticated()){  
+       Campground.findById(req.params.id,function(err,foundCampground){
+           if(err){
+               res.redirect("back");
+           } else{
+               //does user own the campground
+               // console.log(foundCampground.author.id);//mongoose object id
+               // console.log(req.user._id);//string   ,.hence string not(===) mongoose object
+               if(foundCampground.author.id.equals(req.user._id)){
+                   next( );
+               } else{
+                   req.flash("error","You don't have the permission to do that "); 
+                   res.redirect("back");
+               }  
+           }
+       }); 
+   } else {
+         req.flash("error","yOu need to be logged in to do that "); 
+         res.redirect("back");
+   }    
+}
+
+middlewareObj.checkCommentOwnership = function(req,res,next){
+    //is user logged in?
+   if(req.isAuthenticated()){  
+       Comment.findById(req.params.comment_id,function(err,foundComment){
+           if(err){
+               req.flash("error","Campground not found"); 
+               res.redirect("back");
+           } else{
+               //does user own the comment
+               // console.log(foundCampground.author.id);//mongoose object id
+               // console.log(req.user._id);//string   ,.hence string not(===) mongoose object
+               if(foundComment.author.id.equals(req.user._id)){
+                   next( );
+                } else{
+                   req.flash("error","You don't have the permission to do that");
+                   res.redirect("back");
+               }  
+           }
+       }); 
+   } else {
+       req.flash("error","You need to be logged in to do that");
+      res.redirect("back");
+   }    
+}
+
+middlewareObj.isLoggedIn = function(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    req.flash("error","You need to be logged in to do that");
+    res.redirect("/login");
+}
+
+module.exports = middlewareObj;
